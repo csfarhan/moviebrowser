c44207f1c64ae8b61289d555f15d0db07563a314
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-08-03 17:51:07 -0400

    Hero components have been implemented

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return(
+      <header className='bg-dark text-white p-5'>
+        <h1>This is a hero component</h1>
+      </header>
+    )
+  }
+
+  export default Hero;
