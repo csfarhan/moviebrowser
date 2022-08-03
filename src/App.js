a3dccb624453fcd3a4430d5521bb4c2a59e0ded0
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-08-03 16:54:49 -0400

    Imported routing and implemented routes for Home/About

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import AboutView from './components/AboutView';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<AboutView/>}/>
+      </Routes>
     </div>
   );
 }
