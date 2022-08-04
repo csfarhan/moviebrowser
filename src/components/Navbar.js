f84dda1076c6bd44bff5b5dec8d59205d2219875
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-08-03 22:27:02 -0400

    Accessing data from search bar using useState

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,11 @@
 import {Link} from 'react-router-dom';
 
-function Navbar(){
+function Navbar({searchText, setSearchText}){
+
+
+    const updateSearchText = (e) =>{
+      setSearchText(e.target.value)
+    }
     return(
       <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
@@ -21,7 +26,14 @@ function Navbar(){
               </li>
             </ul>
             <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+              <input 
+              className="form-control me-2" 
+              type="search"
+              placeholder="Search" 
+              aria-label="Search"
+              value={searchText}
+              onChange={updateSearchText}
+              ></input>
               <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
           </div>
