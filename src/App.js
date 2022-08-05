9026d49b4a5dade5da922d01927fb7dc1e15ffc5
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-08-04 20:24:00 -0400

    Search functionality fully working with api

--- src/App.js
+++ src/App.js
@@ -10,6 +10,19 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
   const [searchResults,setSearchResults] = useState([]);
   const [searchText, setSearchText] = useState('');
+
+  useEffect(() =>{
+    if(searchText){
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=171a16b6f01fd9f8820affcd7442f63c&language=en-US&query=${searchText}&page=1&include_adult=false`)
+        .then(response => response.json())
+        .then(data=>{
+          setSearchResults(data.results)
+        }) 
+    }
+
+  }, [searchText])
+
+
   return (
     <div>
       <Navbar searchText={searchText} setSearchText={setSearchText}/>
--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,11 +1,45 @@
 import Hero from './Hero';
 
+// api key 171a16b6f01fd9f8820affcd7442f63c
+
+// example search https://api.themoviedb.org/3/search/movie?api_key=171a16b6f01fd9f8820affcd7442f63c&language=en-US&query=star%20wars&page=1&include_adult=false
+
+// example movie 
+
+const MovieCard=({movie})=>{
+  const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+  return(
+    <div className='col-lg-3 col-md-3 col-2 my-4'>
+      <div className="card">
+        <img src={posterUrl} className="card-img-top" alt={movie.original_title}></img>
+        <div className="card-body">
+          <h5 className="card-title">{movie.original_title}</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Show details</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
 const SearchView = ({keyword,searchResults}) => {
+  
   const title = `You are searching for ${keyword}`
-  console.log(searchResults, "are the search results")
+  const resultsHtml = searchResults.map((obj,i) =>{
+    return <MovieCard movie={obj} key={i} />
+  })
+
   return(
     <>
       <Hero text={title} />
+      {resultsHtml &&
+        <div className= 'container'>
+          <div className='row'>
+            {resultsHtml}
+          </div>
+        </div>
+      }
     </>
   )
 }
